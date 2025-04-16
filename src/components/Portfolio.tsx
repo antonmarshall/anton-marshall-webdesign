8737765c7a6851b7c7aabe582d47143152bb8f08
antonmarshall <anton.marshall03@gmail.com>
2025-04-16 16:39:14 +0200

    test

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -51,7 +51,7 @@ const Portfolio = () => {
     {
       id: 'construction',
       title: language === 'de' ? 'Kyoto Teewaren' : 'Kyoto Teaware',
-      image: '${import.meta.env.BASE_URL}/anton-marshall-webdesign/images/bau_unternehmer.png',
+      image: '${import.meta.env.BASE_URL}/images/bau_unternehmer.png',
       alt: language === 'de' ? 'Teegeschirr Shop' : 'Teaware Shop',
       desc: language === 'de' ? 'Bauunternehmer' : 'construction',
       url: 'https://example-kyoto.com',
