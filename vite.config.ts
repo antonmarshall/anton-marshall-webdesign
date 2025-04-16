90eb255dcd5a9a038596a95891456af9e1666a40
antonmarshall <anton.marshall03@gmail.com>
2025-04-16 10:46:48 +0200

    basepfad ändern bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Setze den Basis-Pfad für die Produktion (und den Entwicklungsserver falls gewünscht)
+  base: "/anton-marshall-webdesign/",
   server: {
-    base:"/anton-marshall-webdesign/"
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
